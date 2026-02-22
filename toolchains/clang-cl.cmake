8ac0d421d82582ef588ccc76acfd4276cfc4fb63
expool <gosha.shcherbak@mail.ru>
2026-02-22 15:50:54 +0200

    Updated toolchain files

--- toolchains/clang-cl.cmake
+++ toolchains/clang-cl.cmake
@@ -1,15 +1,20 @@
-set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_VERSION 10.0.x)
+if (WIN32)
+    set(CMAKE_SYSTEM_NAME Windows)
+    set(CMAKE_SYSTEM_VERSION 10.0.x)
 
-set(LLVM_PATH "C:/Program Files/LLVM/bin")
-set(CMAKE_CXX_COMPILER "${LLVM_PATH}/clang-cl.exe")
-set(CMAKE_C_COMPILER "${LLVM_PATH}/clang-cl.exe")
-set(CMAKE_LINKER "${LLVM_PATH}/lld-link.exe")
+    set(LLVM_PATH "C:/Program Files/LLVM/bin")
+    set(CMAKE_CXX_COMPILER "${LLVM_PATH}/clang-cl.exe")
+    set(CMAKE_C_COMPILER "${LLVM_PATH}/clang-cl.exe")
+    set(CMAKE_LINKER "${LLVM_PATH}/lld-link.exe")
+
+    set(CMAKE_CXX_COMPILER_ID "Clang")
+    set(CMAKE_CXX_SIMULATE_ID "MSVC")
+    set(CMAKE_CXX_COMPILER_FRONTEND_VARIANT "MSVC")
+elseif(UNIX)
+    set(CMAKE_SYSTEM_NAME Linux)
+    set(CMAKE_CXX_COMPILER clang-cl)
+    set(CMAKE_C_COMPILER clang-cl)
+    set(CMAKE_LINKER lld-link)
+endif()
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-set(CMAKE_CXX_COMPILER_ID "Clang")
-set(CMAKE_CXX_SIMULATE_ID "MSVC")
-set(CMAKE_CXX_COMPILER_FRONTEND_VARIANT "MSVC")
-
-
 add_compile_options("-Wno-ignored-attributes")
